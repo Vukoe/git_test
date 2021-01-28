892ca43e7515c89c671812846a946b369da786b9
Jaspero <kacper.devek@gmail.com>
2021-01-28 13:42:41 +0100

    First commit

---
+++ hello.c
@@ -0,0 +1,5 @@
+//
+// Created by Kacper on 28.01.2021.
+//
+
+#include "hello.h"
---
+++ hello.h
@@ -0,0 +1,8 @@
+//
+// Created by Kacper on 28.01.2021.
+//
+
+#ifndef GIT_TEST_HELLO_H
+#define GIT_TEST_HELLO_H
+
+#endif //GIT_TEST_HELLO_H
